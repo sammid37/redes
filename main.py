df30425c1c4538e8728f19708c64a4e7d7e35ab1
sammid37 <samanthadmedeiros37@gmail.com>
2024-05-01 09:54:39 -0300

    🪆 Arquivo principaç
    Co-authored-by: EnthonyMiguel <EnthonyMiguel@users.noreply.github.com>

--- main.py
+++ main.py
@@ -8,23 +8,26 @@ import sys
 import time
 sys.path.append('src')
 
+from utils import limpar_tela
 from termcolor import colored
 from udp_impl import UDP_Client
 # from raw_impl import RAW_Client
 
+SERVER_NAME = "15.228.191.109"
+SERVER_PORT = 50000
+
 class ServerFactory:
-  def create_server(self, protocol):
-    if protocol == "UDP":
-      return UDP_Client("15.228.191.109",50000)
-    elif protocol == "RAW":
+  def create_server(self, protocolo, identificador):
+    if protocolo == "UDP":
+      return UDP_Client(SERVER_NAME,SERVER_PORT, identificador)
+    elif protocolo == "RAW":
       # return RAW_Server()
       pass
     else:
       raise ValueError("Protocolo não suportado")
 
 def menuMsg() -> None:
-  print(colored("Bem-vindo ao servidor!","blue"))
-  print(colored((("-"*8) + " MENU"),"blue"))
+  print(colored("🪆 BEM-VINDO!","black", "on_white", attrs=["bold"]))
   print("1 - Data e hora atual")
   print("2 - Mensagem motivacional")
   print("3 - Qtd. de requisições atendidas")
@@ -32,20 +35,25 @@ def menuMsg() -> None:
 
 while True:
   try:
+    limpar_tela()
     factory = ServerFactory()
     menuMsg()
+
     protocolo = input("Digite o protocolo (UDP ou RAW): ").upper()
     opcao = int(input("Digite o número do método (1, 2 ou 3): "))
-  
+    identificador = int(input("Digite um identificador (entre 1 e 65535): "))
+    if identificador < 1 or identificador > 65535:
+      print("Identificador inválido. Por favor, escolha um número entre 1 e 65535.")
+
     if opcao == 4:
       print("Encerrando...")
       server.close()
       break
 
-    server = factory.create_server(protocolo)
+    # Cria um objeto de cliente de protocolo utilizando
+    # o padrão Factory
+    server = factory.create_server(protocolo, identificador)
     server.execute_method(opcao)
-    # time.sleep(3)
-    # os.system("clear") # Unix -> clear, Windows -> cls
-  
+
   except ValueError as e:
     print(colored(("Erro:", e), "red"))
