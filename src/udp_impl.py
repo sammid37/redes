a3a88573ded7aecf171567a206c1412e33e1ce0a
sammid37 <samanthadmedeiros37@gmail.com>
2024-05-01 22:57:57 -0300

    Atualização: (teste) main e UDP Client
    Co-authored-by: EnthonyMiguel <EnthonyMiguel@users.noreply.github.com>
    Co-authored-by: calect <calect@users.noreply.github.com>

--- src/udp_impl.py
+++ src/udp_impl.py
@@ -25,12 +25,53 @@ class UDP_Client:
     """Formata a requisição em Bytes"""
     return bytes([0b0000 << 4 | req_type, identifier >> 8, identifier & 0xFF])
 
+  # def parse_response(self, response:bytes):
+  #   res_type = response[0] >> 4 # Desloca 4 bits para a direita
+  #   identifier = (response[0] & 0x0F) << 8 | response[1]
+  #   response_size = response[2]
+  #   response_data = response[3:3+response_size]
+  #   print(response)
+  #   return res_type, identifier, response_data.decode('utf-8')
+
+  # def parse_response(self, response:bytes):
+  #   res_type = response[0] >> 4  # Desloca 4 bits para a direita para o tipo de resposta
+  #   identifier = (response[0] & 0x0F) << 8 | response[1]  # Combina partes do identificador
+  #   response_size = response[2]  # Tamanho dos dados de resposta
+  #   response_data = response[3:3+response_size]  # Dados da resposta
+  #   print(response)
+  #   print(res_type)
+  #   print(identifier)
+  #   print(response_size)
+  #   print(response_data)
+  #   # Checa se os dados contêm apenas bytes numéricos (0-9)
+  #   if all(48 <= byte <= 57 for byte in response_data[:-1]):  # Ignora o último caractere '%'
+  #       # Converte os bytes numéricos para int, então para string
+  #       data_string = str(int.from_bytes(response_data[:-1], byteorder='big'))
+  #   else:
+  #       # Decodifica como string UTF-8
+  #       data_string = response_data.decode('utf-8', errors='replace')
+
+  #   return res_type, identifier, data_string
+  
   def parse_response(self, response:bytes):
-    res_type = response[0] >> 4 # Desloca 4 bits para a direita
+    res_type = response[0] >> 4
     identifier = (response[0] & 0x0F) << 8 | response[1]
     response_size = response[2]
     response_data = response[3:3+response_size]
-    return res_type, identifier, response_data.decode('utf-8', errors='ignore')
+
+    print(res_type)
+    print(identifier)
+    print(response_size)
+    print(response_data)
+
+    # Assume que os dados são um inteiro de 32 bits
+    if response_size == 4:
+        number_of_requests = int.from_bytes(response_data, byteorder='big')
+        data_string = str(number_of_requests)
+    else:
+        data_string = response_data.decode('utf-8', errors='replace')
+
+    return res_type, identifier, data_string
 
   def execute_method(self, opcao:int) -> None:
     """Executa um dos métodos do servidor dada a opção escolhida pelo usuário e exibe a resposta"""
