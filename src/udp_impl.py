e0de3b5c3c046c5f8c152350f93028f8d2e758ac
sammid37 <samanthadmedeiros37@gmail.com>
2024-05-09 01:33:40 -0300

    🪆 Implementação UDP

--- src/udp_impl.py
+++ src/udp_impl.py
@@ -1,8 +1,10 @@
-import time
-from utils import limpar_tela
+# Redes de Computadores I 
+# Projeto Final - Implementação UDP
+# Christopher Alec Tavares, Enthony Miguel Gomes de Araújo e Samantha Dantas Medeiros
+# Maio de 2024
+
 from termcolor import colored
 from socket import socket, AF_INET, SOCK_DGRAM
-from constantes import SERVER_NAME, SERVER_PORT
 
 class UDP_Client:
   def __init__(self, server_name:str, server_port:int, identificador:int) -> None:
@@ -11,13 +13,8 @@ class UDP_Client:
     self.identificador = identificador
     self.client_socket = socket(AF_INET, SOCK_DGRAM)
 
-  # def f_avancar():
-  #   avancar = input("Pressione ENTER para continuar...")
-  #   if not avancar: 
-  #     limpar_tela()
-
   def send_udp_request(self, request:bytes) -> bytes:
-    """Envia requisição para o servidor"""
+    """Envia requisição para o servidor e retorna a resposta"""
     self.client_socket.sendto(request, (self.server_name, self.server_port))
     response, _ = self.client_socket.recvfrom(80000)
     return response
@@ -27,19 +24,26 @@ class UDP_Client:
     return bytes([0b0000 << 4 | req_type, identifier >> 8, identifier & 0xFF])
 
   def parse_response(self, response:bytes):
-    res_req = response[0] >> 4
-    res_type = response[0] & 0x0F
-    identifier = (response[1] << 8) | response[2]
+    """Analisa a resposta vinda do servidor e a decodifica."""
+    # Primeiro Byte da resposta
+    res_req = response[0] >> 4 # deslocamento à direita do valor por 4 bits
+    res_type = response[0] & 0x0F # operação OR bit a bit utilizando a máscara para extrair os 4 bits
+    # Combinação do Segundo e Terceiro Byte da resposta (or bit a bit), formando um identificador de 16 bits
+    # Deslocando 8 bits à esquerda do Segundo Byte
+    identifier = (response[1] << 8) | response[2] 
     response_size = response[3]
 
     if res_req == 1:  # Verifica se é uma resposta (bits 0001)
+      # agora verifica o tipo de requisição 
+      # (data e hora, frase motivacional ou nº de requisições atendidas)
+      # e decodefica 
       if res_type == 0:  # Requisição de data e hora
         response_data = response[4:].decode('utf-8')
-      elif res_type == 1:  # Requisição de frase motivacional
+      elif res_type == 1:
         response_data = response[4:].decode('utf-8')
-      elif res_type == 2:  # Requisição de número de respostas
+      elif res_type == 2:
         response_data = int.from_bytes(response[4:], byteorder='big', signed=False)
-      elif res_type == 3:  # Requisição inválida
+      elif res_type == 3:
         response_data = "Requisição inválida"
       else:
         response_data = "Tipo de resposta desconhecido"
@@ -49,7 +53,9 @@ class UDP_Client:
     return res_type, identifier, response_data
 
   def execute_method(self, opcao:int) -> None:
-    """Executa um dos métodos do servidor dada a opção escolhida pelo usuário e exibe a resposta"""
+    """Executa um dos métodos do servidor dada a opção escolhida pelo usuário e exibe a resposta.
+    Inicialmente formantando a requisição para então enviá-la para o servidor e finalmente exibir
+    a resposta solicitação do usuário """
     if opcao == 1:
       request = self.format_request(0b0000, self.identificador)
     elif opcao == 2:
@@ -64,14 +70,13 @@ class UDP_Client:
 
       res_type, idt, data = self.parse_response(response)
 
+      # Estamos utilizando esta lista puramente para facilitar a exibição da resposta
       tipo_msg = ["","Data e hora", "Frase motivacional", "Nº de requisições atendidas"]
-      print(colored("\nResposta recebida!", "green"))
-      print(f"{tipo_msg[opcao]}: {data}")
+      print(colored("\n✅ Resposta recebida!", "green"))
+      print(f"{tipo_msg[opcao]}: {data}\n")
 
-      # self.f_avancar()
     except Exception as e:
       print(colored(("Erro ao enviar/receber dados:", e),"red"))
-      # self.f_avancar()
 
   def close(self):
     """Fecha a conexão do cliente"""
