9d6d930bfcf64f526ec1538902753dc855dfd3be
sammid37 <samanthadmedeiros37@gmail.com>
2024-05-09 01:32:55 -0300

    📃 Constantes

--- src/constantes.py
+++ src/constantes.py
@@ -1,2 +1,7 @@
+# Redes de Computadores I 
+# Projeto Final - Constantes
+# Christopher Alec Tavares, Enthony Miguel Gomes de Araújo e Samantha Dantas Medeiros
+# Maio de 2024
+
 SERVER_NAME = "15.228.191.109"
 SERVER_PORT = 50000
